7d9ab3341dde46bc1918d61cb104bba91d5205e5
James Hunter <james@bjorkman.ca>
2017-01-05 08:24:25 +1100

    Update xform_point.js

--- src/__lib__/xform_point.js
+++ src/__lib__/xform_point.js
@@ -3,13 +3,24 @@
 *   | d e f | * | y | = | dx + ey + fz |
 *   | g h i |   | z |   | gx + hy + iz |
 *
-*   | M11 M21 M31 |   | x |   | M11x + M21y + M31 |
-*   | M12 M22 M32 | * | y | = | M12x + M22y + M32 |
-*   | 0   0   1   |   | 1 |   | 0    + 0    + 1   |
-*
 *   | 1 0 5 |   | 3 |   | 8 |
 *   | 0 1 6 | * | 1 | = | 7 |
 *   | 0 0 1 |   | 1 |   | 1 |
+*
+*   -------------------------------------
+*
+*   import xform from "./xform_point"
+*
+*   xform([1,0,0,1,5,6])({ x:3, y:1, type:"M" })
+*   // { x:8, y:7, type:"M" }
+*
+*   xform([1,0,0,1,5,6])({ x:3, y:1, type:"L" })
+*   // { x:8, y:7, type:"L" }
+*
+*   xform([1,0,0,1,5,6])({ type:"Z" })
+*   // { type:"Z" }
+*
+*   -------------------------------------
 *
 *   https://developer.mozilla.org/en/docs/Web/SVG/Attribute/transform
 */
