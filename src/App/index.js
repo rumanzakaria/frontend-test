2879eede727d96986b51ec84eec6d0d2f7cc2567
James Bjorkman <james@bjorkman.ca>
2017-01-05 08:45:10 +1100

    FIX: aspect ratio and svg dims are the same and locked at 900x900

--- src/App/index.js
+++ src/App/index.js
@@ -5,7 +5,7 @@ import axs   from "../annotation_api_data"
 
 export default _props =>
   <div className={ css.app }>
-    <svg className={ css.canvas } viewBox="0 0 600 900">
+    <svg className={ css.canvas } viewBox="0 0 900 900">
       { axs.map(ax => <Ax key={ ax.Id } { ...ax }/>) }
     </svg>
   </div>
